184bcf9cacbae859fb48c2ccc76c8b925029c03f
Caylies <selbydarryl05@gmail.com>
2026-09-14 23:10:03 -0400

    Add rarity field to crates

--- crates/admin.py
+++ crates/admin.py
@@ -15,14 +15,14 @@ if TYPE_CHECKING:
 
 @admin.register(Crate)
 class CrateAdmin(admin.ModelAdmin):
-    list_display = ("name", "emoji", "amount_min", "amount_max", "openable")
-    list_editable = ("amount_min", "amount_max", "openable")
+    list_display = ("name", "emoji", "rarity", "amount_min", "amount_max", "openable")
+    list_editable = ("rarity", "amount_min", "amount_max", "openable")
     search_fields = ("name",)
     autocomplete_fields = ("reward", "pools")
 
     fieldsets = (
-        (None, {"fields": ("name", "emoji_id", "openable")}),
-        ("Rewarding", {"fields": ("amount_min", "amount_max", "reward", "pools")}),
+        (None, {"fields": ("name", "emoji_id", "rarity", "openable")}),
+        ("Rewarding", {"fields": ("reward", "amount_min", "amount_max", "pools")}),
     )
 
     @admin.display(description="Emoji")
